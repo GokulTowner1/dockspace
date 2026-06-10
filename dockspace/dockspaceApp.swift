6578b6e91719b8003b9af4e59c505aec0badbe4d
Gokul <rxnowapp@gmail.com>
2026-06-10 11:17:01 +0530

    Initial Commit

---
+++ dockspace/dockspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  dockspaceApp.swift
+//  dockspace
+//
+//  Created by gokul on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct dockspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
